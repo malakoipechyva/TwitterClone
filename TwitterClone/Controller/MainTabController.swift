720793d76d3cd777d04473ac073eaf7ad64de51f
Yauhen  Chyrko <chirko.eugene@gmail.com>
2021-01-20 18:31:08 +0300

    add root ViewController

---
+++ TwitterClone/Controller/MainTabController.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabController.swift
+//  TwitterClone
+//
+//  Created by malakoipechyva on 19.01.21.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      view.backgroundColor = .systemGreen
+
+    }
+
+}
