e07199d9f12d5c3b8a40bf207f0574327fa68fa6
Yauhen  Chyrko <chirko.eugene@gmail.com>
2021-01-22 14:27:23 +0300

    add project assets

--- TwitterClone/Controller/MainTabController.swift
+++ TwitterClone/Controller/MainTabController.swift
@@ -27,9 +27,13 @@ class MainTabController: UITabBarController {
   func configureViewControllers() {
     
     let feed = FeedController()
+    feed.tabBarItem.image = UIImage(named: "home_unselected")
     let explore = ExploreController()
+    explore.tabBarItem.image = UIImage(named: "search_unselected")
     let notifications = NotificationsController()
+    notifications.tabBarItem.image = UIImage(named: "like_unselected")
     let conversations = ConversationsController()
+    conversations.tabBarItem.image = UIImage(named: "ic_mail_outline_white_2x-1")
     
     viewControllers = [feed, explore, notifications, conversations]
     
