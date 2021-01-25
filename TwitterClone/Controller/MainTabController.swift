750c66896bd1d86c9bb5c2c132da854be83ae8f6
Yauhen  Chyrko <chirko.eugene@gmail.com>
2021-01-25 10:14:15 +0300

    add navigation controllers to the screens

--- TwitterClone/Controller/MainTabController.swift
+++ TwitterClone/Controller/MainTabController.swift
@@ -27,16 +27,26 @@ class MainTabController: UITabBarController {
   func configureViewControllers() {
     
     let feed = FeedController()
-    feed.tabBarItem.image = UIImage(named: "home_unselected")
+    let nav1 = templateNavigationController(image: UIImage(named: "home_unselected"), rootViewController: feed)
+  
     let explore = ExploreController()
-    explore.tabBarItem.image = UIImage(named: "search_unselected")
+    let nav2 = templateNavigationController(image: UIImage(named: "search_unselected"), rootViewController: explore)
+    
     let notifications = NotificationsController()
-    notifications.tabBarItem.image = UIImage(named: "like_unselected")
+    let nav3 = templateNavigationController(image: UIImage(named: "like_unselected"), rootViewController: notifications)
+    
     let conversations = ConversationsController()
-    conversations.tabBarItem.image = UIImage(named: "ic_mail_outline_white_2x-1")
+    let nav4 = templateNavigationController(image: UIImage(named: "ic_mail_outline_white_2x-1"), rootViewController: conversations)
     
-    viewControllers = [feed, explore, notifications, conversations]
+    viewControllers = [nav1, nav2, nav3, nav4]
     
+  }
+  
+  func templateNavigationController(image: UIImage?, rootViewController: UIViewController) -> UINavigationController {
+    let nav = UINavigationController(rootViewController: rootViewController)
+    nav.tabBarItem.image = image
+    nav.navigationBar.barTintColor = .white
+    return nav
   }
 
 }
