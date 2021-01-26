a9f6705a9cc040ef68db47480993d6eb2019f7a5
Yauhen  Chyrko <chirko.eugene@gmail.com>
2021-01-26 12:59:42 +0300

    add authentication stack view

--- TwitterClone/Controller/Authentication/LoginController.swift
+++ TwitterClone/Controller/Authentication/LoginController.swift
@@ -19,6 +19,29 @@ class LoginController: UIViewController {
     return iv
   }()
   
+  private lazy var emailContainerView: UIView = {
+    let image =   #imageLiteral(resourceName: "ic_mail_outline_white_2x-1")
+    let view = Utilities().inputContainerView(withImage: image, textField: emailTextField)
+    return view
+  }()
+  
+  private lazy var passwordContainerView: UIView = {
+    let image = #imageLiteral(resourceName: "ic_lock_outline_white_2x")
+    let view = Utilities().inputContainerView(withImage: image,textField: passwordTextField)
+    return view
+  }()
+  
+  private let emailTextField: UITextField = {
+    let tf = Utilities().textField(withPlaceholder: "Email")
+    return tf
+  }()
+  
+  private let passwordTextField: UITextField = {
+    let tf = Utilities().textField(withPlaceholder: "Password")
+    tf.isSecureTextEntry = true
+    return tf
+  }()
+  
   //MARK: - Lifecycle
   
   override func viewDidLoad() {
@@ -39,5 +62,13 @@ class LoginController: UIViewController {
     view.addSubview(logoImageView)
     logoImageView.centerX(inView: view, topAnchor: view.safeAreaLayoutGuide.topAnchor)
     logoImageView.setDimensions(width: 150, height: 150)
+    
+    let stack = UIStackView(arrangedSubviews: [emailContainerView, passwordContainerView])
+    stack.axis = .vertical
+    stack.spacing = 8
+    
+    view.addSubview(stack)
+    stack.anchor(top: logoImageView.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor,
+                 paddingLeft: 16, paddingRight: 16)
   }
 }
