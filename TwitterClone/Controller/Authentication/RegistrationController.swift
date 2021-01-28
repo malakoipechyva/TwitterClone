fc928741ad55d7a5791f0ce22d121ce803d46624
Yauhen  Chyrko <chirko.eugene@gmail.com>
2021-01-28 13:20:39 +0300

    add profile photo selection

--- TwitterClone/Controller/Authentication/RegistrationController.swift
+++ TwitterClone/Controller/Authentication/RegistrationController.swift
@@ -11,6 +11,8 @@ class RegistrationController: UIViewController {
   
   //MARK: - Properties
   
+  private let imagePicker = UIImagePickerController()
+  
   private let plusPhotoButton: UIButton = {
     let button = UIButton(type: .system)
     button.setImage(UIImage(named: "plus_photo"), for: .normal)
@@ -92,7 +94,7 @@ class RegistrationController: UIViewController {
   //MARK: - Selectors
   
   @objc func handleAddProfilePhoto() {
-    print("Add photo...")
+    present(imagePicker, animated: true, completion: nil)
   }
   
   @objc func handleRegistration() {
@@ -107,6 +109,9 @@ class RegistrationController: UIViewController {
   
   func configureUI() {
     view.backgroundColor = .twitterBlue
+    
+    imagePicker.delegate = self
+    imagePicker.allowsEditing = true
     
     view.addSubview(plusPhotoButton)
     plusPhotoButton.centerX(inView: view, topAnchor: view.safeAreaLayoutGuide.topAnchor)
@@ -127,4 +132,25 @@ class RegistrationController: UIViewController {
                                  bottom: view.safeAreaLayoutGuide.bottomAnchor,
                                  right: view.rightAnchor, paddingLeft: 40, paddingRight: 40)
   }
+}
+
+//MARK: - UIImagePickerControllerDelegate
+
+extension RegistrationController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+
+        guard let profileImage = info[.editedImage] as? UIImage else { return }
+        self.plusPhotoButton.setImage(profileImage.withRenderingMode(.alwaysOriginal), for: .normal)
+        plusPhotoButton.layer.cornerRadius = 128 / 2
+        plusPhotoButton.layer.masksToBounds = true
+        plusPhotoButton.imageView?.contentMode = .scaleToFill
+        plusPhotoButton.imageView?.clipsToBounds = true
+        plusPhotoButton.layer.borderColor = UIColor.white.cgColor
+        plusPhotoButton.layer.borderWidth = 3
+
+        dismiss(animated: true, completion: nil)
+
+    }
+
 }
