832ef0000ce51c4796abcc6ab9acc8f931ccdaf1
Yauhen  Chyrko <chirko.eugene@gmail.com>
2021-02-02 17:25:50 +0300

    add user model

---
+++ TwitterClone/Model/User.swift
@@ -0,0 +1,27 @@
+//
+//  User.swift
+//  TwitterClone
+//
+//  Created by malakoipechyva on 2.02.21.
+//
+
+import Foundation
+
+struct User {
+    
+    let fullname: String
+    let email: String
+    let username: String
+    let profileImageUrl: String
+    let uid: String
+    
+    init(uid: String, dictionary: [String: AnyObject]) {
+        self.uid = uid
+        
+        self.fullname = dictionary["fullname"] as? String ?? ""
+        self.email = dictionary["email"] as? String ?? ""
+        self.username = dictionary["username"] as? String ?? ""
+        self.profileImageUrl = dictionary["profileImageUrl"] as? String ?? ""
+    }
+    
+}
