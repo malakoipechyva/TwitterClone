783fdbab7954a1a746a9f7d55537e42651da7dbb
Yauhen  Chyrko <chirko.eugene@gmail.com>
2021-02-02 16:41:06 +0300

    add fetching user data from firebase

---
+++ TwitterClone/API/UserService.swift
@@ -0,0 +1,26 @@
+//
+//  UserService.swift
+//  TwitterClone
+//
+//  Created by malakoipechyva on 1.02.21.
+//
+
+import Firebase
+
+struct UserService {
+    
+    static let shared = UserService()
+    
+    func fetchUser() {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        
+        REF_USERS.child(uid).observeSingleEvent(of: .value) { (snapshot) in
+            print("DEBUG: Snapshot is \(snapshot)")
+            guard let dictionary = snapshot.value as? [String: AnyObject] else { return }
+            
+            guard let username = dictionary["username"] as? String else { return }
+            print("DEBUG: Username is \(username)")
+            
+        }
+    }
+}
