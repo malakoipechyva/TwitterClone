b35b3af039852bfa864a16a72f622f94d79769ff
Yauhen  Chyrko <chirko.eugene@gmail.com>
2021-02-03 13:01:09 +0300

    add load user profile image

--- TwitterClone/API/UserService.swift
+++ TwitterClone/API/UserService.swift
@@ -11,14 +11,13 @@ struct UserService {
     
     static let shared = UserService()
     
-    func fetchUser() {
+    func fetchUser(completion: @escaping(User) -> Void) {
         guard let uid = Auth.auth().currentUser?.uid else { return }
         
         REF_USERS.child(uid).observeSingleEvent(of: .value) { (snapshot) in
             guard let dictionary = snapshot.value as? [String: AnyObject] else { return }
             let user = User(uid: uid, dictionary: dictionary)
-            print("DEBUG: Username is \(user.username)")
-            print("DEBUG: Fullname is \(user.fullname)")
+            completion(user)
             
         }
     }
