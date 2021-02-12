0db4f1f46412207099be6709aacb0b47dcd58b99
Yauhen  Chyrko <chirko.eugene@gmail.com>
2021-02-12 19:05:12 +0300

    add search page ui

--- TwitterClone/Controller/ExploreController.swift
+++ TwitterClone/Controller/ExploreController.swift
@@ -7,7 +7,9 @@
 
 import UIKit
 
-class ExploreController: UIViewController {
+private let reuseIdentidier = "UserCell"
+
+class ExploreController: UITableViewController {
     
     //MARK: - Properties
     
@@ -24,7 +26,21 @@ class ExploreController: UIViewController {
     
     func configureUI() {
         view.backgroundColor = .white
-        
         navigationItem.title = "Explore"
+        
+        tableView.register(UserCell.self, forCellReuseIdentifier: reuseIdentidier)
+        tableView.rowHeight = 60
+        tableView.separatorStyle = .none
+    }
+}
+
+extension ExploreController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentidier, for: indexPath) as! UserCell
+        return cell
     }
 }
---
+++ TwitterClone/View/UserCell.swift
@@ -0,0 +1,65 @@
+//
+//  UserCell.swift
+//  TwitterClone
+//
+//  Created by malakoipechyva on 12.02.21.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell {
+    
+    //MARK: - Properties
+    
+    private lazy var profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.setDimensions(width: 40, height: 40)
+        iv.layer.cornerRadius = 40 / 2
+        iv.backgroundColor = .twitterBlue
+        
+        return iv
+    }()
+    
+    private let usernameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        label.text = "Username"
+        return label
+    }()
+    
+    private let fullnameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.text = "Fullname"
+        return label
+    }()
+    
+    //MARK: - Lifecycle
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(profileImageView)
+        profileImageView.centerY(inView: self, leftAnchor: leftAnchor, paddingLeft: 12)
+        
+        let stack = UIStackView(arrangedSubviews: [usernameLabel, fullnameLabel])
+        stack.axis = .vertical
+        stack.spacing = 2
+        
+        addSubview(stack)
+        stack.centerY(inView: profileImageView, leftAnchor: profileImageView.rightAnchor, paddingLeft: 12)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Selectors
+    
+    //MARK: - API
+    
+    //MARK: - Helpers
+    
+}
