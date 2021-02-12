9a724f538f9981bd1b14878fe0f0757d96a08f24
Yauhen  Chyrko <chirko.eugene@gmail.com>
2021-02-12 12:11:14 +0300

    add fetch user tweets

--- TwitterClone/Controller/ProfileController.swift
+++ TwitterClone/Controller/ProfileController.swift
@@ -16,6 +16,11 @@ class ProfileController: UICollectionViewController {
     
     private let user: User
     
+    private var tweets = [Tweet]() {
+        didSet {
+            collectionView.reloadData()        }
+    }
+    
     //MARK: - Lifecycle
     
     init(user: User) {
@@ -30,12 +35,21 @@ class ProfileController: UICollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configureCollectionView()
+        fetchTweets()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.navigationBar.barStyle = .black
         navigationController?.navigationBar.isHidden = true
+    }
+    
+    //MARK: - API
+    
+    func fetchTweets() {
+        TweetService.shared.fetchTweets(forUser: user) { tweets in
+            self.tweets = tweets
+        }
     }
     
     //MARK: - Helpers
@@ -49,16 +63,15 @@ class ProfileController: UICollectionViewController {
     }
 }
 
-//MARK: - UICollectionViewDataSource
-
 extension ProfileController {
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 3
+        return tweets.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier,
                                                       for: indexPath) as! TweetCell
+        cell.tweet = tweets[indexPath.row]
         return cell
     }
 }
