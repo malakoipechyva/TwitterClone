31d3f3a5f76046ef248da4b9e1336bdc96b09a97
Yauhen  Chyrko <chirko.eugene@gmail.com>
2021-02-22 11:48:57 +0300

    add uploading reply to firebase

--- TwitterClone/Controller/UploadTweetController.swift
+++ TwitterClone/Controller/UploadTweetController.swift
@@ -69,12 +69,6 @@ class UploadTweetController: UIViewController {
         super.viewDidLoad()
         configureUI()
         
-        switch config {
-        case .tweet:
-            print("DEBUG: Config is tweet")
-        case .reply(let tweet):
-            print("DEBUG: Config is replying to \(tweet.caption)")
-        }
     }
     
     //MARK: - Selectors
@@ -85,7 +79,7 @@ class UploadTweetController: UIViewController {
     
     @objc func handleUploadTweet() {
         guard let caption = captionTextView.text else { return }
-        TweetService.shared.uploadTweet(caption: caption) { (error, ref) in
+        TweetService.shared.uploadTweet(caption: caption, type: config) { (error, ref) in
             if let error = error {
                 print("DEBUG: Failed to upload tweet with error - \(error.localizedDescription)")
                 return
