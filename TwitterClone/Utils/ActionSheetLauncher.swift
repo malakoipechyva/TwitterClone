7ae9182e7856fefb4ac3a1434e1b66073a0cf96b
Yauhen  Chyrko <chirko.eugene@gmail.com>
2021-02-22 21:38:46 +0300

    add table view setup

--- TwitterClone/Utils/ActionSheetLauncher.swift
+++ TwitterClone/Utils/ActionSheetLauncher.swift
@@ -5,19 +5,25 @@
 //  Created by malakoipechyva on 22.02.21.
 //
 
-import Foundation
+import UIKit
+
+private let reuseIdentifier = "ActionSheetCell"
 
 class ActionSheetLauncher: NSObject {
     
     //MARK: - Properties
     
     private let user: User
+    private let tableView = UITableView()
+    private var window: UIWindow?
     
     //MARK: - Lifecycle
     
     init(user: User) {
         self.user = user
         super.init()
+        
+        configureTableView()
     }
     
     //MARK: - Selectors
@@ -27,7 +33,43 @@ class ActionSheetLauncher: NSObject {
     //MARK: - Helpers
     
     func show() {
-        print("DEBUG: Show action sheet for user \(user.username)")
+        guard let window = UIApplication.shared.windows.first(where: { $0.isKeyWindow }) else { return }
+        self.window = window
+        
+        window.addSubview(tableView)
+        tableView.frame = CGRect(x: 0, y: window.frame.height - 300, width: window.frame.width, height: 300)
+        
     }
+    
+    func configureTableView() {
+        tableView.backgroundColor = .red
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.rowHeight = 60
+        tableView.separatorStyle = .none
+        tableView.layer.cornerRadius = 5
+        tableView.isScrollEnabled = false
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
+    }
+}
+
+//MARK: - UITableViewDataSource
+
+extension ActionSheetLauncher: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        return cell
+    }
+}
+
+//MARK: - UITableViewDelegate
+
+extension ActionSheetLauncher: UITableViewDelegate {
     
 }
