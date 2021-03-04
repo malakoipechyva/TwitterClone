d8907273493dcfa56984f4e95ce6610d0857f027
Yauhen  Chyrko <chirko.eugene@gmail.com>
2021-03-04 16:17:07 +0300

    add fetch user replies

--- TwitterClone/Controller/ProfileController.swift
+++ TwitterClone/Controller/ProfileController.swift
@@ -50,6 +50,7 @@ class ProfileController: UICollectionViewController {
         configureCollectionView()
         fetchTweets()
         fetchLikedTweets()
+        fetchReplies()
         checkIfUserIsFollowed()
         fetchUserStats()
     }
@@ -72,6 +73,12 @@ class ProfileController: UICollectionViewController {
     func fetchLikedTweets() {
         TweetService.shared.fetchLikes(forUser: user) { tweets in
             self.likedTweets = tweets
+        }
+    }
+    
+    func fetchReplies() {
+        TweetService.shared.fetchReplies(forUser: user) { tweets in
+            self.replies = tweets
         }
     }
     
@@ -137,7 +144,7 @@ extension ProfileController: UICollectionViewDelegateFlowLayout {
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let viewModel = TweetViewModel(tweet: tweets[indexPath.row])
+        let viewModel = TweetViewModel(tweet: currentDataSource[indexPath.row])
         let height = viewModel.size( forWidth: view.frame.width).height
         
         return CGSize(width: view.frame.width, height: height + 72)
