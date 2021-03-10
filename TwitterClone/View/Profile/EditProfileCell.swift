34e2f8e385b381bb76055f1a2a64777180574edd
Yauhen  Chyrko <chirko.eugene@gmail.com>
2021-03-10 19:49:38 +0300

    add custom text view

--- TwitterClone/View/Profile/EditProfileCell.swift
+++ TwitterClone/View/Profile/EditProfileCell.swift
@@ -40,10 +40,11 @@ class EditProfileCell: UITableViewCell {
         return tf
     }()
     
-    let bioTextView: UITextView = {
-        let tv = UITextView()
+    let bioTextView: InputTextView = {
+        let tv = InputTextView()
         tv.font = UIFont.systemFont(ofSize: 14)
         tv.textColor = .twitterBlue
+        tv.placeholderLabel.text = "Bio"
         return tv
     }()
     
